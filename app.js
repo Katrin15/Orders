3bddee8c96df48a133280bed361608d75c0ffbd2
Katrin <aniretakeko@gmail.com>
2016-08-01 20:26:40 +0300

    sort all table columns az za

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 (function(){
 	var app = angular.module("orders", [ ]);
 
+	/*
 	app.controller("SendOrderController", function($scope, $http, $httpParamSerializerJQLike) {
 
 		this.order = {
@@ -9,6 +10,7 @@
 			projectName: "Арктические медведи",
 			orderUrgency: "low"
 		};
+
 
 		this.addOrder = function() {
 
@@ -24,7 +26,57 @@
 				  }
 				}).then(function(res){
 	         		$scope.mimi = res.data;
-	         		console.log(res.data);	             
+	         		console.log(res.data);
+	         		clearOrder();	             
+	    		});
+					//success(function(res) { 
+					// do something here
+					//console.log("ура"); 
+					//console.log(res);
+					//console.log(order2);
+				//});
+		};
+
+	});
+	*/
+
+	app.controller("SendOrderController", function($rootScope, $scope, $http, $httpParamSerializerJQLike) {
+
+		$scope.order = {
+			theme: "",
+			themeText: "",
+			projectName: "Арктические медведи",
+			orderUrgency: "низкая"
+		};
+
+
+		$scope.addOrder = function() {
+			
+			$http({
+				  url: 'addOrder.php',
+				  method: 'POST',
+				  data: $httpParamSerializerJQLike($scope.order), // Make sure to inject the service you choose to the controller
+				  headers: {
+				    'Content-Type': 'application/x-www-form-urlencoded' // Nte the appropriate header
+				  }
+				}).success(function(res) { //then(function(res){ console.log(res.data);
+	    
+	         		console.log(res);
+
+	         		$rootScope.orders.unshift($scope.order); //при успешном ответе от сервера - добавить заявку в таблицу
+	         		console.log($scope.orders);
+
+	         		$scope.order = {
+						theme: "",
+						themeText: "",
+						projectName: "Арктические медведи",
+						orderUrgency: "низкая"
+					};
+	         		$scope.sendOrder.$setPristine();
+	         		$scope.sendOrder.$setUntouched(); //очистить форму после отправки
+
+
+
 	    		});
 					//success(function(res) { 
 					// do something here
@@ -36,16 +88,50 @@
 
 	});
 
+
 	
 	
-	app.controller("TableDataController", function($scope, $http) {
+	app.controller("TableDataController", function($rootScope, $http) {
 
 
 	    $http.get("orders.json").then(function(res){
-	         $scope.orders = res.data;	             
+	         //$scope.orders = res.data;	
+	         $rootScope.orders = res.data;  
 	    });
+
+	    // не нужно this.sortType = "theme"; //вначале сортировка по теме по алфавиту
+	    //this.sortReverse = 1; // ini
+	    this.sortReverseI = [];
+
+	    this.sortColumn = function(sortType, i) {
+	    	this.sortType = sortType;
+	    	this.sortReverseI[i] = !this.sortReverseI[i];
+	    	
+	    	this.sortReverse = this.sortReverseI[i];
+
+	    	//console.log(this.sortReverseI);
+	    	//console.log(this.sortReverse);
+	    }
 	    
 	});
+	
+
+
+
+
+	/*
+	app.controller("TableDataController", [ "$http", function($http) {
+		var orders = this;
+
+	    $http.get("orders.json").success(function(res){
+	         orders = res;
+	         console.log(res);
+	         console.log(orders); // все равно не видно в html (?)	             
+	    });
+	    
+	}]);
+	*/
+
 	
 
 
